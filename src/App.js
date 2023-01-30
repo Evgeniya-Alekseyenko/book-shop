e83083bb7cd6daf0cfb74658c55f4032c051600a
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2023-01-30 12:46:11 +0200

    Added NotFoundPage component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Signin from './components/Signin/Signin.jsx';
+// import Header from './components/Header.jsx';
+// import Footer from './components/Footer.jsx';
+// import Signin from './components/Signin/Signin.jsx';
+import NotFoundBlock from './components/NotFoundPage/NotFoundPage.jsx';
 
 import './scss/app.scss';
 
 function App() {
     return (
         <div className='wrapper'>
-            <Header />
+            {/* <Header />
             <Signin />
-            <Footer />
+            <Footer /> */}
+            <NotFoundBlock />
         </div>
     );
 }
---
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import styles from './NotFoundPage.module.scss';
+
+const NotFoundPage = () => {
+    return (
+        <div className={styles.root}>
+            <h1>Not Found 😑</h1>
+            <p>Oops, something went wrong. 404 error”</p>
+        </div>
+    );
+};
+export default NotFoundPage;
